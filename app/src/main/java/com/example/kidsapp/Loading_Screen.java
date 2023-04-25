1be90cb5ca3dce5d598ae3f95d9f157c856c7a2b
Ouhachi Fares Mohamed Amine <Ouhachifares@gmail.com>
2023-04-25 16:46:43 +0100

    Splash screen animation works fine

---
+++ app/src/main/java/com/example/kidsapp/Loading_Screen.java
@@ -0,0 +1,26 @@
+package com.example.kidsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Loading_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(this, Home.class);
+            startActivity(intent);
+            finish();
+        }, 3900);
+
+
+    }
+}
