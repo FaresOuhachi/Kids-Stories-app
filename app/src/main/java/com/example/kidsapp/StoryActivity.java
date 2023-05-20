9179d04f0fef90ae1b4741b75534fa316ccf6096
Ouhachi Fares Mohamed Amine <Ouhachifares@gmail.com>
2023-05-21 00:18:55 +0100

    onClick works fine and textToSpeech too

---
+++ app/src/main/java/com/example/kidsapp/StoryActivity.java
@@ -0,0 +1,69 @@
+package com.example.kidsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class StoryActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    TextView text;
+    TextToSpeech textToSpeech;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+        text = findViewById(R.id.story_text);
+        textToSpeech = new TextToSpeech(this, this);
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = textToSpeech.setLanguage(Locale.FRENCH);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "Language not supported");
+            } else {
+                // Text-to-speech is ready
+                speakOut(text.getText().toString());
+            }
+        } else {
+            Log.e("TTS", "Initialization failed");
+        }
+    }
+
+    private void speakOut(String text) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (textToSpeech != null && text != null) {
+            speakOut(text.getText().toString());
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+    }
+
+}
